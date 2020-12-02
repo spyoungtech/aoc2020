1cf09b5a2ef9eaddf8a192f93dd4b86ccf874ab1
Spencer Young <spencer.young@spyoung.com>
2020-12-01 18:56:56 -0800

    day1 parts 1 and 2

---
+++ day1/day1.py
@@ -0,0 +1,37 @@
+import itertools
+import operator
+
+
+def load():
+    with open('input.txt') as f:
+        numbers = [int(line.strip()) for line in f if line]
+    return numbers
+
+
+def find_entries(numbers, goal, r):
+    for combo in itertools.combinations(numbers, r):
+        if sum(combo) == goal:
+            return combo
+
+def product(*numbers):
+    i = None
+    for i in itertools.accumulate(numbers, operator.mul):
+        ...
+    return i
+
+def _main(goal, r):
+    numbers = load()
+    entries = find_entries(numbers, goal=goal, r=r)
+    return product(*entries)
+
+
+def part_1():
+    return _main(goal=2020, r=2)
+
+
+def part_2():
+    return _main(goal=2020, r=3)
+
+if __name__ == '__main__':
+    print(part_1())
+    print(part_2())
