fb171b598d212db056a07def1f36f111a1bb49e1
Spencer Young <spencer.young@spyoung.com>
2020-12-03 10:36:45 -0800

    day 2 part 2

--- day2/day2.py
+++ day2/day2.py
@@ -1,4 +1,6 @@
-from typing import Tuple
+from typing import Tuple, Type
+
+
 class Policy:
     def __init__(self, minimum: int, maximum: int, alpha: str):
         self.range = range(minimum, maximum+1)
@@ -10,25 +12,40 @@ class Policy:
         return False
 
 
-def parse_line(line: str) -> Tuple[Policy, str]:
+class PositionalPolicy(Policy):
+    def validate(self, password: str):
+        index_positions = (self.range.start-1, self.range.stop-2)
+        try:
+            index_values = ''.join(password[index] for index in index_positions)
+        except IndexError:
+            return False
+        if index_values.count(self.alpha) != 1:
+            return False
+        else:
+            return True
+
+
+def parse_line(line: str, policy_type: Type[Policy]) -> Tuple[Policy, str]:
     line = line.strip()
     policy_part, password = line.split(': ')
     quantifier, alpha = policy_part.split()
     min_quantity, max_quantity = quantifier.split('-')
-    policy = Policy(int(min_quantity), int(max_quantity), alpha)
+    policy = policy_type(int(min_quantity), int(max_quantity), alpha)
     return policy, password
 
-def parse_input(fp='input.txt'):
+def parse_input(fp='input.txt', policy_type=Policy):
     with open(fp) as input_file:
-        return [parse_line(line) for line in input_file if line.strip()]
+        return [parse_line(line, policy_type=policy_type) for line in input_file if line.strip()]
 
 
-def main(fp='input.txt'):
+def main(fp='input.txt', policy_type=Policy):
     valid_count = 0
-    for policy, password in parse_input(fp):
+    for policy, password in parse_input(fp, policy_type=policy_type):
         if policy.validate(password):
             valid_count += 1
     return valid_count
 
+
 if __name__ == '__main__':
-    print(main())
+    print(main())  # part 1
+    print(main(policy_type=PositionalPolicy))  # part 2
